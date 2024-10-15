8c182b858d53bfd1384e879ada8b960cac8a4128
doranl <doranl@educ.college-montreal.qc.ca>
2024-10-15 11:05:37 -0400

    V3 (final)

--- TP3.py
+++ TP3.py
@@ -14,7 +14,8 @@ nombre_victoires = 0
 nombre_defaites = 0
 nombre_victoires_consecutives = 0
 jeu = True
-while jeu == True:  # le début du jeu
+
+while jeu:  # le début du jeu
     if nombre_victoires_consecutives == 3:
         force_adversaire = random.randint(4, 5) + random.randint(4, 5)
         print("Vous tombez face à face avec un boss de difficulté : %d"
@@ -41,17 +42,22 @@ while jeu == True:  # le début du jeu
         print("Lancer du dé : %d"
               % score_de)
         time.sleep(1)
+
         if score_de <= force_adversaire:  # défaite du jouer
             niveau_vie -= force_adversaire
+
             if niveau_vie <= 0:  # la fin du partie
                 print("La partie est terminée, vous avez vaincu %d monstre(s)."
                       % nombre_victoires)
                 time.sleep(1)
                 jouer = str(input("voulez-vous rejouer?\n"))
+
                 if jouer == "oui":
                     print(" ")
+
                 else:
-                    break
+                    jeu = False
+
             else:
                 nombre_defaites += 1
                 print("défaite\n"
@@ -59,6 +65,7 @@ while jeu == True:  # le début du jeu
                       % niveau_vie)
                 print(nombre_defaites)
                 time.sleep(1)
+
         elif score_de > force_adversaire:  # victoire du joueur
             nombre_victoires += 1
             nombre_victoires_consecutives += 1
@@ -68,11 +75,13 @@ while jeu == True:  # le début du jeu
                   "Nombre de victoires consécutives : %d"
                   % (niveau_vie, nombre_victoires_consecutives))
             time.sleep(1)
+
     elif int(choix) == 2:  # contourner l'adversaire
         print("vous contournez cet adversaire et ouvre une autre porte")
         niveau_vie -= 1
         print("Niveau de vie : %d" % niveau_vie)
         time.sleep(3)
+
     elif int(choix) == 3:  # les règles
         print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.  "
               "Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
@@ -83,9 +92,12 @@ while jeu == True:  # le début du jeu
               "L’usager peut combattre ou éviter chaque adversaire, "
               "dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
         time.sleep(3)
+
     elif int(choix) == 4:  # quitter le jeu
         jouer = str(input("voulez-vous quitter?\n"))
+
         if jouer == "oui":
-            break
+            jeu = False
+
         else:
             print("\n")
